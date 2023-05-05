65d6590ebedca43eb88f5f651e6a2bcc046746db
Ahmedgamalebied <safaagamalebied@gmail.com>
2023-05-05 04:34:45 +0300

    custom app bar created

---
+++ lib/views/Notes_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:my_diary/views/Widgets/Cutom_appBar.dart';
+import 'package:my_diary/views/Widgets/note-view-body.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: NotesviewBody(),
+    );
+  }
+}
