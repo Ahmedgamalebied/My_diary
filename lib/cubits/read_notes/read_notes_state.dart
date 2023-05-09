b1bb328be3ea12a4b40d91a6718e2f136324b72e
Ahmedgamalebied <safaagamalebied@gmail.com>
2023-05-09 19:14:16 +0300

    create fetch notes method

---
+++ lib/cubits/read_notes/read_notes_state.dart
@@ -0,0 +1,20 @@
+part of 'read_notes_cubit.dart';
+
+@immutable
+abstract class ReadNotesState {}
+
+class ReadNotesInitial extends ReadNotesState {}
+
+class ReadNotesSucess extends ReadNotesState {
+  final List<NoteModel> notes;
+
+  ReadNotesSucess(this.notes);
+}
+
+class ReadNotesLoading extends ReadNotesState {}
+
+class ReadNotesFailure extends ReadNotesState {
+  final String errMessage;
+
+  ReadNotesFailure(this.errMessage);
+}
