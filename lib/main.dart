082f5cdc90b43acde2c13e871953aff427fa5ed3
Ahmedgamalebied <safaagamalebied@gmail.com>
2023-05-09 18:14:34 +0300

    bloc provider and handle expection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:my_diary/cubits/add_notes/add_notes_cubit.dart';
+import 'package:my_diary/simple_bloc_observer.dart';
 import 'package:my_diary/views/Notes_view.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'constants.dart';
@@ -8,8 +9,10 @@ import 'models/note_model.dart';
 
 void main() async {
   await Hive.initFlutter();
-  await Hive.openBox(kNotesBox);
+  Bloc.observer = SimpleBlocObserver();
   Hive.registerAdapter(NoteModelAdapter());
+
+  await Hive.openBox<NoteModel>(kNotesBox);
   runApp(const DailyApp());
 }
 
@@ -17,17 +20,10 @@ class DailyApp extends StatelessWidget {
   const DailyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AddNotesCubit(),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-        home: const NotesView(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+      home: const NotesView(),
     );
   }
 }
